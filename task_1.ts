89c189297a6713e3287cea305bad437c5d0add21
tanyaIlina4199 <4199ti@gmail.com>
2023-05-15 22:53:27 +0300

    added homework 4

--- task_1.ts
+++ task_1.ts
@@ -1,6 +1,27 @@
-let x = 20
-let y = 58
-let z = 42
+let a = "Таня"
+let b = "Ильина"
+console.log(`Привет ${a.toLocaleUpperCase()} ${b.toLocaleUpperCase()}`)// все большие
+console.log(`Привет ${a.toLocaleLowerCase()} ${b.toLocaleLowerCase()}`)// все маленькие
+console.log(`Привет ${a.toLocaleUpperCase()} ${b.toLocaleLowerCase()}`)// большое+маленькое
+console.log(`Привет ${a.toLocaleLowerCase()} ${b.toLocaleUpperCase()}`)//маленькое+большое
+console.log(`Привет ${changeRegister(a)} ${changeRegister(b)}`)
 
-let sum = x + y + z
-console.log(sum)
+let fullPhrase = "Привет ".concat(a).concat(" ").concat(b);
+let full = fullPhrase.charAt(3);
+
+console.log(fullPhrase);
+
+function changeRegister(str: String) {
+    let new_str = "";
+    let len = str.length;
+      for (let i = 0; i < len; i++) {
+        if (i % 2 === 0) {
+          new_str += str[i].toUpperCase();
+        } else {
+          new_str += str[i].toLowerCase();
+        }
+      }
+      return new_str;
+}
+
+//`Привет <Имя> <Фамилия>`
