b366ae3117ad7e71e36e6b42ce3de1a09dcee4bf
tanyaIlina4199 <4199ti@gmail.com>
2023-05-22 21:38:19 +0300

    removed old files

--- task_8.ts
+++
@@ -1,3 +0,0 @@
-let xx: number = 8.829734872948
-let yy: string = xx.toString().substring(0,5)
-console.log(Number.parseFloat(yy))
--- task_9.ts
+++
@@ -1,2 +0,0 @@
-let aa: number = 6.0876876876
-console.log(Number.isInteger(aa) ? aa : Math.round(aa));
