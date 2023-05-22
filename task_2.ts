48508a5adec4d6d95f9a37c99e5fff8bc637934a
tanyaIlina4199 <4199ti@gmail.com>
2023-05-22 21:35:39 +0300

    fixed task 2

--- task_2.ts
+++ task_2.ts
@@ -3,15 +3,17 @@
 // (0.17*credit)*5 - pereplata
 
 function pereplata(credit: number): number {
-    //return credit * 0.17 * 5;
-    let perepalta = 0;
-    const yearly = credit / 5;
-    for (let i = 1; i <= 5; i++) {
-        perepalta = perepalta + credit * 0.17;
-        credit = credit - yearly;
-        i++;
-    }
-
-    return perepalta;
+    return credit * 0.17 * 5;
 }
-console.log(pereplata(1))
+console.log(pereplata(2000))
+
+// second solution
+// let perepalta = 0;
+//     const yearly = credit / 5;
+//     for (let i = 1; i <= 5; i++) {
+//         perepalta = perepalta + credit * 0.17;
+//         credit = credit - yearly;
+//         i++;
+//     }
+
+//     return perepalta;
